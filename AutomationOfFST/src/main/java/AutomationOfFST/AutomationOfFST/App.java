10c6a8a7266ba3644068457db18d6f1aa0ff980a
Vishal.Chugh <Vishal.Chugh@IN01N03442.icepor.com>
2020-11-23 18:30:00 +0530

    First

---
+++ AutomationOfFST/src/main/java/AutomationOfFST/AutomationOfFST/App.java
@@ -0,0 +1,13 @@
+package AutomationOfFST.AutomationOfFST;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
