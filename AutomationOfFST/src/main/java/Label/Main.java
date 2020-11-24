29a56f6289bc851c2366fd24fcd72c32a764ecb1
Vishal.Chugh <Vishal.Chugh@IN01N03442.icepor.com>
2020-11-24 11:35:20 +0530

    Final2

--- AutomationOfFST/src/main/java/Label/Main.java
+++ AutomationOfFST/src/main/java/Label/Main.java
@@ -8,8 +8,10 @@ public class Main {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/drivers/chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
 		driver.get("https://qa2.slots.partyslots.de/de/slots");
 		System.out.println(driver.getTitle());
+		driver.quit();
 	}
 
 }
